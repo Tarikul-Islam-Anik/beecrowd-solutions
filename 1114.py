17a47591b10b2b70e5d22ac6474329b3a59815f2
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2022-06-20 21:47:09 +0600

    Solved!

---
+++ 1114.py
@@ -0,0 +1,7 @@
+while True:
+    password = int(input())
+    if password == 2002:
+        print("Acesso Permitido")
+        break
+    else:
+        print("Senha Invalida")
---
+++ 1173.py
@@ -0,0 +1,6 @@
+n = int(input())
+print(f"N[{0}] = {n}")
+
+for i in range(1, 10):
+    n *= 2
+    print(f"N[{i}] = {n}")
